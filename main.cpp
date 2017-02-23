bf02a9b5b1aae0845038d2344b6f3d737df98ee4
olimpiamarin <olimpiamarin84@gmail.com>
2017-02-23 14:03:42 +0200

    Exercitiul 2

--- main.cpp
+++ main.cpp
@@ -9,11 +9,26 @@ int main()
     rachete*/
     // use Sleep() pentru a opri aplicatia pentru 1000
     // milisecunde
-
+    /*
     for(int i = 10; i > 0; i--){
         cout << i << endl;
         Sleep(1000); // temporizare 1s
     }
     cout << "Racheta lansata." << endl;
     return 0;
+    */
+
+    /* Creaţi un program care iniţializează cu 0 valorile elementelor unui şir*/
+
+    int a[20], i;
+    for (i = 0; i < 20; i++){
+        a[i] = i + 1;
+    }
+    for(i = 0; i < 20; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+
+
 }
