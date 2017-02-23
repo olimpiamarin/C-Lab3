4993d5819c488a165b4912cfaae2856bef51c6a4
olimpiamarin <olimpiamarin84@gmail.com>
2017-02-23 13:57:49 +0200

    Exercitiul 1

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*Realizați o aplicație care realizează
+    numărătoarea inversă care precede lansarea unei
+    rachete*/
+
+    for(int i = 10; i > 0; i--){
+        cout << i << endl;
+    }
+    cout << "Racheta lansata." << endl;
+    return 0;
+}
