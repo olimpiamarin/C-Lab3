b55d35528560d6f224b4decf322441b691944d3b
olimpiamarin <olimpiamarin84@gmail.com>
2017-02-23 14:23:28 +0200

    Exercitiul 3

--- main.cpp
+++ main.cpp
@@ -19,13 +19,37 @@ int main()
     */
 
     /* Creaţi un program care iniţializează cu 0 valorile elementelor unui şir*/
-
+    /*
     int a[20], i;
     for (i = 0; i < 20; i++){
         a[i] = 19 - i;
     }
     for(i = 0; i < 20; i++){
         cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+    */
+
+    /*Creaţi un program care citeşte valorile unui şir de valori întregi a şi apoi
+     schimbă semnul elementelor negative, respectiv dacă a[i] < 0 atunci a[i] = -a[i].
+    Observaţie: În aplicaţiile următoare referitoare la şiruri de valori puteţi să nu
+    mai citiţi şirul iniţial (ia cam mult timp!) iniţializând şirul în momentul
+    declarării după modelul*/
+
+    int a[20], nr, i;
+    cout << "Numarul de elemente din sirul a (maxim 20)";
+    cin >> nr;
+    for (i = 0; i < nr; i++){
+        cout << "a[" << i << "] = ";
+        cin >> a[i];
+    }
+    for (i = 0; i < nr; i++){
+        if(a[i] < 0)
+            a[i] = -a[i];
+    }
+    for (i = 0; i < nr; i++){
+        cout << a[i] << ", ";
     }
     cout << endl;
     return 0;
