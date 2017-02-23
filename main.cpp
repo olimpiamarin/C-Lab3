7f885a0dc1dc12f3e381c9d3fbea7cb5bf389434
olimpiamarin <olimpiamarin84@gmail.com>
2017-02-23 13:59:30 +0200

    Exercitiul 1-1

--- main.cpp
+++ main.cpp
@@ -7,9 +7,12 @@ int main()
     /*Realizați o aplicație care realizează
     numărătoarea inversă care precede lansarea unei
     rachete*/
+    // use Sleep() pentru a opri aplicatia pentru 1000
+    // milisecunde
 
     for(int i = 10; i > 0; i--){
         cout << i << endl;
+        Sleep(1000); // temporizare 1s
     }
     cout << "Racheta lansata." << endl;
     return 0;
